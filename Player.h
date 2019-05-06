c718c9a4b8c1978e22ef7b9ed3389df6f22a147c
Zach Nett <zjnett@mac.com>
2019-05-06 12:24:48 -0700

    Wrote Player header

--- Player.h
+++ Player.h
@@ -10,13 +10,24 @@ File: Player.h
 #include "Game.h"
 #include "Entity.h"
 
-class Player : public Entity {
+class Player : public Entity, public sf::RectangleShape {
 public:
-    Player() {
-        
+    // Value Constructor 1
+    Player(string name, double health, double maxHealth, double stamina, double maxStamina, 
+        sf::Vector2f &size, sf::Texture &texture, sf::Vector2f &pos) : name(name), health(health), 
+        maxHealth(maxHealth), stamina(stamina), maxStamina(maxStamina), sf::RectangleShape(size) {
+            setTexture(texture);
+            setPosition(position);
+        }
+    
+    // Value Constructor 2
+    Player(sf::Vector2f &size, sf::Texture &texture, sf::Vector2f &pos) : sf::RectangleShape(size) {
+        setTexture(texture);
+        setPosition(pos);
     }
-private:
 
+    // Destructor
+    ~Player() {}
 };
 
 #endif /* PLAYER_H */
